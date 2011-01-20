49062da95595e5f531b8f30aa5931941533fd832
VvDPzZ <vvdpzz@gmail.com>
2011-01-20 21:10:25 +0800

    将自己提问和回答的问题移至个人页面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,7 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @records = @user.records
+        @questions = current_user.questions
+        @answers = current_user.answers
     end
 end
