85aa2475bd504fa2b02e375beba03dea2fc9ad16
VvDPzZ <vvdpzz@gmail.com>
2011-01-22 16:32:42 +0800

    修复了顶部导航栏的链接问题

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,22 @@ module ApplicationHelper
     def nwp(money)
         number_with_precision(money.to_f/100,:precision => 2)
     end
+    
+    class NavTabBuilder < TabsOnRails::Tabs::Builder
+      def tab_for(tab, name, options, item_options = {})
+        item_options[:class] = (current_tab?(tab) ? 'active' : 'inactive')
+        @context.content_tag(:li) do
+          @context.link_to(name, options,item_options)
+        end
+      end
+      
+      def open_tabs(options = {})
+        @context.tag("ul", options, open = true)
+      end
+
+      def close_tabs(options = {})
+        "</ul>".html_safe
+      end
+    end
+    
 end
