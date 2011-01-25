07f55858930ae7311974be78f19f869a25e57bfe
VvDPzZ <vvdpzz@gmail.com>
2011-01-25 19:18:18 +0800

    dont know how to

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,22 @@ module ApplicationHelper
     def nwp(money)
         number_with_precision(money.to_f/100,:precision => 2)
     end
+    
+    class NavTabBuilder < TabsOnRails::Tabs::Builder
+      def tab_for(tab, name, options, item_options = {})
+        item_options[:class] = (current_tab?(tab) ? 'active' : nil)
+        @context.content_tag(:li, item_options) do
+          @context.link_to(name, options)
+        end
+      end
+      
+      def open_tabs(options = {})
+        @context.tag("ul", options, open = true)
+      end
+
+      def close_tabs(options = {})
+        "</ul>".html_safe
+      end
+    end
+    
 end
