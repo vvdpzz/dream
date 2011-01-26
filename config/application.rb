40b0d8a2727071006148be1531fee6724541e093
VvDPzZ <vvdpzz@gmail.com>
2011-01-26 20:02:04 +0800

    try mongodb

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,20 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'uri'
-if ENV['MONGOHQ_URL']
-  mongo_uri = URI.parse(ENV['MONGOHQ_URL'])
-  ENV['MONGOID_HOST'] = mongo_uri.host
-  ENV['MONGOID_PORT'] = mongo_uri.port.to_s
-  ENV['MONGOID_USERNAME'] = mongo_uri.user
-  ENV['MONGOID_PASSWORD'] = mongo_uri.password
-  ENV['MONGOID_DATABASE'] = mongo_uri.path.gsub('/', '')
-end
+# require 'uri'
+# if ENV['MONGOHQ_URL']
+#   mongo_uri = URI.parse(ENV['MONGOHQ_URL'])
+#   ENV['MONGOID_HOST'] = mongo_uri.host
+#   ENV['MONGOID_PORT'] = mongo_uri.port.to_s
+#   ENV['MONGOID_USERNAME'] = mongo_uri.user
+#   ENV['MONGOID_PASSWORD'] = mongo_uri.password
+#   ENV['MONGOID_DATABASE'] = mongo_uri.path.gsub('/', '')
+# end
+
+  ENV['MONGOID_HOST'] = "localhost"
+  ENV['MONGOID_PORT'] = "27017"
+  ENV['MONGOID_USERNAME'] = "mongodb"
+  ENV['MONGOID_PASSWORD'] = ""
+  ENV['MONGOID_DATABASE'] = "dream"
 
 require 'mongoid/railtie'
 require "action_controller/railtie"
